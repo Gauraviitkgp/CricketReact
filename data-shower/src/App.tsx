32015e1189ef07469cf34ffef88cd9d0b70366dc
Gauraviitkgp <gauravs123456789@gmail.com>
2022-12-20 17:26:39 +0900

    Added loading screen

--- data-shower/src/App.tsx
+++ data-shower/src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import config from "./local.config.json";
 import DataTable from "./model/table";
+import { Button } from "@mui/material";
 
 interface PlayerName {
 	faceImageId: string;
@@ -28,79 +29,88 @@ const options = {
 	},
 };
 
-async function getPlayerID(name: string): Promise<PlayerNames> {
+async function getPlayerID(name: string): Promise<Response> {
 	const url = config.base_url + "player/search?plrN=" + name;
 
-	console.log(url);
+	return fetch(url, options);
 
-	return await new Promise((resolve, reject) => {
-		setTimeout(() => {
-			fetch(url, options)
-				.then((res) => resolve(res.json()))
-				.catch((err) => reject("error:" + err));
-		}, config.defaultTimeOut);
-	});
+	// return new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		fetch(url, options)
+	// 			.then((res) => resolve(res.json()))
+	// 			.catch((err) => reject("error:" + err));
+	// 	}, config.defaultTimeOut);
+	// });
 }
 
-async function getPlayerData(params: string): Promise<PlayerData> {
+async function getPlayerData(params: string): Promise<Response> {
 	const url = config.base_url + "player/" + params + "/batting";
 
-	return await new Promise((resolve, reject) => {
-		setTimeout(() => {
-			fetch(url, options)
-				.then((res) => resolve(res.json()))
-				.catch((err) => reject("error:" + err));
-		}, config.defaultTimeOut);
-	});
+	return fetch(url, options);
+
+	// return new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		fetch(url, options)
+	// 			.then((res) => resolve(res.json()))
+	// 			.catch((err) => reject("error:" + err));
+	// 	}, config.defaultTimeOut);
+	// });
+}
+
+// async function getFinalOutput(name: string): Promise<PlayerData>{
+// 	let playerID = await (await getPlayerID(name)).json() as PlayerNames;
+
+// }
+
+function LoadingIcon(props: {isLoading:boolean, values: PlayerData }):JSX.Element{
+	if (props.isLoading){
+		return <>LOADING</>
+	}
+	
+	return <DataTable rows={props.values.values}></DataTable>
 }
 
 function App(props: { playerName: string[] }) {
-	// const [data, setData] = React.useState<PlayerData>({
-	// 	headers: [],
-	// 	values: [{ values: [] }],
-	// });
+	const [data, setData] = React.useState<PlayerData>({
+		headers: [],
+		values: [{ values: [] }],
+	});
 
-	let data:PlayerData = {
-			headers: [],
-			values: [{ values: [] }],
-		};
-		
-	getPlayerID(props.playerName[0]);
-	getPlayerData("1413");
-	// getPlayerID(props.playerName[0])
-	// 	.then((players) => {
-	// 		getPlayerData(players.player[0].id)
-	// 			.then((data1) => {
-	// 				// setData(data);
-	// 				data = data1;
-	// 			})
-	// 			.catch((err) => {
-	// 				console.log("Some error occured:" + err);
-	// 			});
-	// 	})
-	// 	.catch((err) => {
-	// 		console.log("Some error occured:" + err);
-	// 	});
+	const [gotData, setGotData] = React.useState<boolean>(false);
+
+	const refreshData = () => {
+		let playerIDRes = getPlayerID(props.playerName[0]).then((res) => {
+			{
+				return res.json() as Promise<PlayerNames>;
+			}
+		});
+
+		playerIDRes.then((players) => {
+			getPlayerData(players.player[0].id)
+				.then(async (data1) => {
+					console.log(data1);
+					setData((await data1.json()) as PlayerData);
+					setGotData(true);
+				})
+				.catch((err) => {
+					console.log("Some error occured:" + err);
+				});
+		});
+	};
+
+	React.useEffect(() => {
+		if (!gotData){
+			refreshData();
+		}
+	});
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
 			<div>
-				<DataTable rows={data.values}></DataTable>
+				<LoadingIcon isLoading={!gotData} values={data}></LoadingIcon>
 			</div>
+
+			<button onClick={refreshData}>Refresh</button>
 		</div>
 	);
 }
