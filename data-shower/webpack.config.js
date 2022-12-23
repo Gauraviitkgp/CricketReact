8cc64f698d3da4256e78159d8f59cdc9270b91d6
Gauraviitkgp <gauravs123456789@gmail.com>
2022-12-23 17:14:26 +0900

    Started module federation on Datashower

--- data-shower/webpack.config.js
+++ data-shower/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
 
 module.exports = {
+	output: {
+		publicPath: "http://localhost:3001/",
+	},
+	devServer: {
+		port: 3001,
+		historyApiFallback: true,
+	},
 	entry: "./src/index.tsx",
-    mode:"development",
-    devtool: 'inline-source-map',
+	mode: "development",
+	devtool: "inline-source-map",
 	module: {
 		rules: [
 			{
@@ -12,39 +22,43 @@ module.exports = {
 				use: "ts-loader",
 				exclude: /node_modules/,
 			},
-            {
-                test: /\.(css|s[ac]ss)$/i,
-                use: ["style-loader", "css-loader", "postcss-loader"],
-              },
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
 		],
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js",".css",".json"],
+		extensions: [".tsx", ".ts", ".js", ".css", ".json"],
 	},
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
 	},
-    plugins: [
-        // new ModuleFederationPlugin({
-        //   name: "monolith",
-        //   filename: "remoteEntry.js",
-        //   remotes: {},
-        //   exposes: {},
-        //   shared: {
-        //     ...deps,
-        //     react: {
-        //       singleton: true,
-        //       requiredVersion: deps.react,
-        //     },
-        //     "react-dom": {
-        //       singleton: true,
-        //       requiredVersion: deps["react-dom"],
-        //     },
-        //   },
-        // }),
-        new HtmlWebPackPlugin({
-          template: "./public/index.html",
-        }),
-      ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "datashower",
+			filename: "remoteEntry.js",
+			remotes: {
+                datashower:"datashower"
+            },
+			exposes: {
+				"./Shower": "./src/App.tsx",
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./public/index.html",
+		}),
+	],
 };
